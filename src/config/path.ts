d7a94a111950f151992906f426f30858a6505d14
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-13 19:51:16 +0300

    feat(config): add base config files

---
+++ src/config/path.ts
@@ -0,0 +1,9 @@
+export enum Path {
+  HOME = "/",
+  TRENDS = "/trends",
+  FAVORITES = "/favorites",
+  SETTINGS = "/settings",
+  LOGIN = "/login",
+  REGISTRATION = "/registration",
+  MOVIE = "/movie/",
+}
