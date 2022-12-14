d60cf32e4f8c18beac145f0ba4eb61bf09232bc0
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-14 16:53:28 +0300

    feat(components): add logo img for sidebar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,11 +1,14 @@
 import { MenuNav } from "components";
 import { StyledSideBar, Image, Copyright } from "./styles";
-import logo from "../../assets/images/logo.png";
+import logoLight from "assets/img/logo-light.png";
+import logoDark from "assets/img/logo-dark.png";
+import { getTheme, useAppSelector } from "store";
 
 export const SideBar = () => {
+  const { theme } = useAppSelector(getTheme);
   return (
     <StyledSideBar>
-      <Image src={logo} />
+      <Image src={theme === "dark" ? logoLight : logoDark} />
       <MenuNav />
       <Copyright>© All Rights Reserved</Copyright>
     </StyledSideBar>
