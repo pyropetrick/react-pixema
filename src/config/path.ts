f69909d0d3fa0a9b339283c6eac66c6f8f37c82c
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-15 23:34:21 +0300

    refactor(config): delete old file path

--- src/config/path.ts
+++
@@ -1,9 +0,0 @@
-export enum Path {
-  HOME = "/",
-  TRENDS = "/trends",
-  FAVORITES = "/favorites",
-  SETTINGS = "/settings",
-  LOGIN = "/login",
-  REGISTRATION = "/registration",
-  MOVIE = "/movie/",
-}
