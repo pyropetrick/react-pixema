298c31786b91d9d5dfd39514c12b5b2f31f90616
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-20 19:21:36 +0300

    feat(assets): add barrel index

---
+++ src/assets/index.ts
@@ -0,0 +1,33 @@
+import logoLight from "./img/logo-light.png";
+import logoDark from "./img/logo-dark.png";
+import bgAuth from "./img/background-auth.png";
+import { ReactComponent as ArrowRightIcon } from "./icons/arrow-right.svg";
+import { ReactComponent as ArrowLeftIcon } from "./icons/arrow-left.svg";
+import { ReactComponent as BookMarkIcon } from "./icons/bookmark.svg";
+import { ReactComponent as BurgerIcon } from "./icons/burger-menu.svg";
+import { ReactComponent as CrossIcon } from "./icons/cross.svg";
+import { ReactComponent as FireIcon } from "./icons/fire.svg";
+import { ReactComponent as HomeIcon } from "./icons/home.svg";
+import { ReactComponent as IMDBIcon } from "./icons/imdb.svg";
+import { ReactComponent as SearchFilterIcon } from "./icons/search-filter.svg";
+import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
+import { ReactComponent as ShareIcon } from "./icons/share.svg";
+import { ReactComponent as UserIcon } from "./icons/user.svg";
+
+export {
+  logoLight,
+  logoDark,
+  bgAuth,
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  BookMarkIcon,
+  BurgerIcon,
+  CrossIcon,
+  FireIcon,
+  HomeIcon,
+  IMDBIcon,
+  SearchFilterIcon,
+  SettingsIcon,
+  ShareIcon,
+  UserIcon,
+};
