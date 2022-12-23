e53d9872f6561617f2be2cc1f534f892b5010bd7
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-23 22:42:34 +0300

    fix(ui): fix sizing body

--- src/ui/globalStyles.ts
+++ src/ui/globalStyles.ts
@@ -1,5 +1,5 @@
-import styled, { createGlobalStyle } from "styled-components";
-import { Media, resetCSS, themeDark, themeLight } from ".";
+import { createGlobalStyle } from "styled-components";
+import { resetCSS, themeDark, themeLight } from ".";
 import { Color } from "../config";
 
 export const GlobalStyles = createGlobalStyle`
@@ -8,7 +8,7 @@ ${resetCSS};
 body {
   color: ${Color.PRIMARY_TEXT};
   background-color: ${Color.PRIMARY_BG};
-  height: 100%;
+  /* height: 100%; */
 }
 html {
   font-family: 'Exo 2', sans-serif;
