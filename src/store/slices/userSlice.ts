427b55af825630479425d0a8eab11e081ac9e75a
pyropetrick <pavel.chernenko97@gmail.com>
2022-12-28 00:36:27 +0300

    feat(store): add auth thunks

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,39 +1,97 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  updateProfile,
+  User,
+  UserCredential,
+} from "firebase/auth";
+import { FirebaseError, FirebaseErrorCode, getFirebaseErrorMessage } from "services";
+import { ISignInData, ISignUpData } from "types";
 
 interface IUser {
-  name: string;
-  email: string;
+  name: string | null | undefined;
+  email: string | null | undefined;
   isAuth: boolean;
+  isLoading: boolean;
+  error: string;
 }
-interface IUserAPI {
-  displayName: string | null;
-  email: string | null;
-}
+
+export const signUp = createAsyncThunk<any, ISignUpData, { rejectValue: FirebaseError }>(
+  "user/signUp",
+  async ({ email, password, name }, { rejectWithValue }) => {
+    try {
+      const auth = getAuth();
+      await createUserWithEmailAndPassword(auth, email, password);
+      auth.currentUser &&
+        (await updateProfile(auth.currentUser, {
+          displayName: name,
+        }));
+      return auth.currentUser as User;
+    } catch (error) {
+      const firebaseError = error as { errorCode: FirebaseErrorCode };
+      rejectWithValue(getFirebaseErrorMessage(firebaseError.errorCode));
+    }
+  },
+);
+
+export const signIn = createAsyncThunk<
+  UserCredential | undefined,
+  ISignInData,
+  { rejectValue: FirebaseError }
+>("user/signIn", async ({ email, password }, { rejectWithValue }) => {
+  try {
+    const auth = getAuth();
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    const firebaseError = error as { errorCode: FirebaseErrorCode };
+    rejectWithValue(getFirebaseErrorMessage(firebaseError.errorCode));
+  }
+});
 
 const initialState: IUser = {
   name: "",
   email: "",
   isAuth: false,
+  isLoading: false,
+  error: "",
 };
 
 const user = createSlice({
   name: "user",
   initialState,
-  reducers: {
-    setUser: (state, { payload: { displayName, email } }: PayloadAction<IUserAPI>) => {
-      if (displayName && email) {
-        state.isAuth = true;
-        state.name = displayName;
-        state.email = email;
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(signUp.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(signUp.fulfilled, (state, { payload }) => {
+      state.email = payload.email as string;
+      state.name = payload.displayName as string;
+      state.isAuth = true;
+    });
+    builder.addCase(signUp.rejected, (state, { payload }) => {
+      if (payload) {
+        state.error = payload;
       }
-    },
-    removeUser: (state) => {
-      state.isAuth = false;
-      state.name = "";
-    },
+    });
+    builder.addCase(signIn.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(signIn.fulfilled, (state, { payload }) => {
+      if (payload) {
+        state.email = payload.user.email;
+        state.name = payload.user.displayName;
+        state.isAuth = true;
+      }
+    });
+    builder.addCase(signIn.rejected, (state, { payload }) => {
+      if (payload) {
+        state.error = payload;
+      }
+    });
   },
 });
 
 export default user.reducer;
-
-export const { setUser, removeUser } = user.actions;
