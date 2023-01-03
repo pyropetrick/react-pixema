5c117fe354c29b7247b9e7c61521be4a76eefc27
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-03 19:46:00 +0300

    feat(components): add search logic into header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import { Profile, Search } from "components";
-import { useInput } from "hooks";
+import { useDebounce, useInput } from "hooks";
 import { StyledHeader } from "./styles";
 import { useAppSelector, getUser } from "store";
+import { useEffect } from "react";
+import { generatePath, useNavigate } from "react-router";
+import { ROUTE } from "router";
 
 export const Header = () => {
   const search = useInput();
+  const navigate = useNavigate();
+  const debounceValue = useDebounce(search.value, 1000);
+  useEffect(() => {
+    debounceValue && navigate(generatePath(ROUTE.SEARCH, { name: debounceValue }));
+  }, [debounceValue]);
   const { isAuth, name } = useAppSelector(getUser);
   return (
     <StyledHeader>
