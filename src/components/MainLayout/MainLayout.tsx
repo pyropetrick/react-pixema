40249d9db8d09f0cf4a03e06952cc9bba56ac8ed
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-04 18:25:01 +0300

    feat(components): add toastify to main layout

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,14 +1,30 @@
-import { Header, SideBar } from "components";
+import { Header, Portal, SideBar } from "components";
 import { Outlet } from "react-router-dom";
 import { Content, StyledMainLayout } from "./styles";
 import { useEffect } from "react";
 import { getTheme, useAppSelector } from "store";
+import { ToastContainer } from "react-toastify";
+import { TargetPortal } from "config";
 
 export const MainLayout = () => {
   const { theme } = useAppSelector(getTheme);
   useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
   return (
     <StyledMainLayout>
+      <Portal target={TargetPortal.TOAST}>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme={theme}
+        />
+      </Portal>
       <SideBar />
       <Content>
         <Header />
