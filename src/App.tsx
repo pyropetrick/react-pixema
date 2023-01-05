1316c9b5cf48f46c61c923428a7f9ee738bf74e2
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-06 01:46:05 +0300

    feat(src): add loading for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { LoadingBar } from "components";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { router } from "router";
-import { getTheme, setUser, useAppDispatch, useAppSelector } from "store";
+import { getTheme, getUser, setUser, useAppDispatch, useAppSelector } from "store";
 
 export const App = () => {
   const { theme } = useAppSelector(getTheme);
+  const { isLoading } = useAppSelector(getUser);
   const auth = getAuth();
   const dispatch = useAppDispatch();
   useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
-  useEffect(() => onAuthStateChanged(auth, (user) => dispatch(setUser(user))), []);
+  useEffect(() => onAuthStateChanged(auth, (user) => dispatch(setUser(user))), [dispatch, auth]);
+  if (isLoading) return <LoadingBar />;
   return <RouterProvider router={router} />;
 };
