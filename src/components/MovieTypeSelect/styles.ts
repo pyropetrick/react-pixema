1e9c51c81b7333b4efd99fb311178f07b025361a
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-05 21:01:29 +0300

    refactor(components): delete old styles for select

--- src/components/MovieTypeSelect/styles.ts
+++ src/components/MovieTypeSelect/styles.ts
@@ -1,24 +1,6 @@
 import { StylesConfig } from "react-select";
 import { Color } from "config";
 
-// export const selectStyles: StylesConfig = {
-//   control: (styles) => ({
-//     ...styles,
-//     fontSize: "16px",
-//     fontWeight: "500",
-//     border: "none",
-//     borderRadius: "10px",
-//     backgroundColor: Color.GRAPHITE,
-//   }),
-//   menu: (styles) => ({ ...styles, backgroundColor: Color.GRAPHITE, borderRadius: "10px" }),
-//   singleValue: (styles) => ({ ...styles, color: "#ffffff" }),
-//   container: (styles) => ({ ...styles, backgoundColor: Color.GRAPHITE }),
-//   indicatorSeparator: (styles) => ({
-//     ...styles,
-//     display: "none",
-//   }),
-// };
-
 export const selectStyles: StylesConfig = {
   control: (styles) => ({
     ...styles,
