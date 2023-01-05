d9cca594f75636abbe39a30688d94de904419467
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-05 21:00:11 +0300

    feat(ui): add color theme for select

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -6,10 +6,12 @@ export const themeLight = css`
   --primary-bg: ${Color.WHITE};
   --secondary-bg: ${Color.WHITE};
   --block-bg: ${Color.WHITE};
+  --select-bg: ${Color.LIGHT};
 `;
 export const themeDark = css`
   --primary-text: ${Color.WHITE};
   --primary-bg: ${Color.BLACK};
   --secondary-bg: ${Color.GRAPHITE};
   --block-bg: ${Color.DARK};
+  --select-bg: ${Color.GRAPHITE};
 `;
