5ed749252f3bc0751685ff56ce4cb9028526fc5c
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-05 21:46:46 +0300

    feat(components): add new styles for loading bar

--- src/components/LoadingBar/styles.ts
+++ src/components/LoadingBar/styles.ts
@@ -1,37 +1,59 @@
 import styled from "styled-components";
-import { Media } from "ui";
 
 export const StyledLoadingBar = styled.div`
-  display: flex;
-  align-self: center;
-  justify-self: center;
-  width: 100px;
-  height: 15px;
-  background: linear-gradient(to bottom right, cyan, lightblue);
-  border-radius: 6px;
-  box-shadow: 0 0 10px lightblue;
-  animation: load 1s infinite alternate ease-in-out;
-  @keyframes load {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 5px solid #e9218c;
+  border-radius: 50%;
+  border-left-color: transparent;
+  animation-name: spin;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-play-state: running;
+  &:after {
+    content: "";
+    position: absolute;
+    width: 130%;
+    height: 131%;
+    border: 5px solid #2490fd;
+    border-radius: 50%;
+    border-right-color: transparent;
+    left: -13px;
+    top: -12px;
+    animation-name: spin;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-play-state: running;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    width: 160%;
+    height: 160%;
+    border: 5px solid #fcb100;
+    border-radius: 50%;
+    border-left-color: transparent;
+    left: -20px;
+    top: -20px;
+    animation-name: spin;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-play-state: running;
+  }
+  @keyframes spin {
     from {
-      transform: translate(0, 0);
+      transform: rotate(0deg);
     }
+
     to {
-      transform: translate(390px, 0);
-    }
-  }
-  ${Media.MD} {
-    @keyframes load {
-      to {
-        transform: translate(200px, 0);
-      }
-    }
-  }
-  ${Media.MD} {
-    width: 50px;
-    @keyframes load {
-      to {
-        transform: translate(100px, 0);
-      }
+      transform: rotate(360deg);
     }
   }
 `;
