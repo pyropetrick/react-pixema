cc14793448054c634edb8a523669c2ad0f0b369d
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-05 20:24:54 +0300

    feat(pages): add search params to search page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -2,12 +2,18 @@ import { Button, MovieList } from "components";
 import { useEffect } from "react";
 import { fetchSearchMovies, getSearchMovies, useAppDispatch, useAppSelector } from "store";
 import { useParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
 
 export const SearchPage = () => {
   const { name } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const year = searchParams.get("year");
+  const type = searchParams.get("type");
   const dispatch = useAppDispatch();
   useEffect(() => {
-    name && dispatch(fetchSearchMovies({ name }));
+    if (year && type && name) {
+      dispatch(fetchSearchMovies({ name, year, type }));
+    } else name && dispatch(fetchSearchMovies({ name }));
   }, [dispatch, name]);
   const { movies, isLoading } = useAppSelector(getSearchMovies);
   return (
