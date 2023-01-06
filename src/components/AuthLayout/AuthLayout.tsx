4aa57a0de6dbf16e4b20894fff77799eb119f4bc
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-06 15:06:07 +0300

    fix(components): fix error with loading

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,11 +1,12 @@
 import { StyledAuthLayout, Content, Copyright } from "./styles";
 import { Navigate, Outlet } from "react-router";
-import { Logo } from "components";
+import { LoadingBar, Logo } from "components";
 import { getUser, useAppSelector } from "store";
 import { ROUTE } from "router";
 
 export const AuthLayout = () => {
-  const { name } = useAppSelector(getUser);
+  const { name, isLoading } = useAppSelector(getUser);
+  if (isLoading) <LoadingBar />;
   if (name) {
     return <Navigate to={ROUTE.HOME} />;
   }
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,8 +1,11 @@
-import { Header, SideBar } from "components";
+import { Header, LoadingBar, SideBar } from "components";
 import { Outlet } from "react-router-dom";
+import { getUser, useAppSelector } from "store";
 import { Content, StyledMainLayout } from "./styles";
 
 export const MainLayout = () => {
+  const { isLoading } = useAppSelector(getUser);
+  if (isLoading) <LoadingBar />;
   return (
     <StyledMainLayout>
       <SideBar />
