f470fbd7941b610cab02c7ff3ebe6085390ef83f
pyropetrick <pavel.chernenko97@gmail.com>
2023-01-06 15:06:44 +0300

    fix(src): fix error with loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { LoadingBar } from "components";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { router } from "router";
-import { getTheme, getUser, setUser, useAppDispatch, useAppSelector } from "store";
+import { getTheme, setUser, useAppDispatch, useAppSelector } from "store";
 
 export const App = () => {
   const { theme } = useAppSelector(getTheme);
-  const { isLoading } = useAppSelector(getUser);
   const auth = getAuth();
   const dispatch = useAppDispatch();
   useEffect(() => document.documentElement.setAttribute("theme", theme), [theme]);
   useEffect(() => onAuthStateChanged(auth, (user) => dispatch(setUser(user))), [dispatch, auth]);
-  if (isLoading) return <LoadingBar />;
   return <RouterProvider router={router} />;
 };
