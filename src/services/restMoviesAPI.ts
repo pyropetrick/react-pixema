74201d3b5b76f71c340ac4b9e55e615543815537
DESKTOP-G3EI2ED\Павел <pavel.chernenko97@gmail.com>
2023-01-13 17:34:22 +0300

    feat(services): add endpoints and new requests to api

--- src/services/restMoviesAPI.ts
+++ src/services/restMoviesAPI.ts
@@ -1,34 +1,73 @@
 import axios from "axios";
-import { IResponseSearchAPI, IMovieInfoAPI, IRequestOption } from "types";
+import {
+  IResponseSearchAPI,
+  IMovieInfoAPI,
+  IRequestOption,
+  IResponsePopularMovies,
+  IResponseComingSoonMovies,
+} from "types";
+
+enum ENDPOINT {
+  SEARCH = "AdvancedSearch/",
+  HOME = "Top250Movies/",
+  TRENDS = "MostPopularMovies/",
+  COMING_SOON = "ComingSoon/",
+  MOVIE_BY_ID = "Title/",
+}
 
 class restMoviesAPI {
   private readonly urlAPI = process.env.REACT_APP_API_URL;
   private readonly keyAPI = process.env.REACT_APP_API_KEY;
+  private readonly endpoints = {
+    search: ENDPOINT.SEARCH + this.keyAPI,
+    home: ENDPOINT.HOME + this.keyAPI,
+    trends: ENDPOINT.TRENDS + this.keyAPI,
+    comingSoon: ENDPOINT.COMING_SOON + this.keyAPI,
+    searchByID: ENDPOINT.MOVIE_BY_ID + this.keyAPI,
+  };
   private readonly API = axios.create({
     baseURL: this.urlAPI,
-    params: {
-      apikey: this.keyAPI,
-    },
   });
 
-  public async getMovie(id: string) {
-    const params = {
-      i: id,
-      plot: "full",
-    };
-    const { data } = await this.API.get<IMovieInfoAPI>("", { params });
-    if (data.Error) throw new Error(data.Error);
+  public async getMovieById(id: string) {
+    const params = "Trailer, Wikipedia";
+    const { data } = await this.API.get<IMovieInfoAPI>(
+      `${this.endpoints.searchByID}/${id}/${params}`,
+    );
+    if (data.errorMessage) throw new Error(data.errorMessage);
     return data;
   }
-  public async getSearchMovies({ name, type = null, year = null, page }: IRequestOption) {
+  public async getSearch({
+    title,
+    titleType = null,
+    releaseData = null,
+    genres = null,
+  }: IRequestOption) {
     const params = {
-      s: name,
-      type,
-      y: year,
-      page,
+      title,
+      title_type: titleType,
+      release_data: releaseData,
+      genres,
     };
-    const { data } = await this.API.get<IResponseSearchAPI>("", { params });
-    if (data.Error) throw new Error(data.Error);
+    const { data } = await this.API.get<IResponseSearchAPI>(this.endpoints.search, {
+      params,
+    });
+    if (data.errorMessage) throw new Error(data.errorMessage);
+    return data;
+  }
+  public async getPopularMovies() {
+    const { data } = await this.API.get<IResponsePopularMovies>(this.endpoints.trends);
+    if (data.errorMessage) throw new Error(data.errorMessage);
+    return data;
+  }
+  public async getHomeMovies() {
+    const { data } = await this.API.get<IResponsePopularMovies>(this.endpoints.home);
+    if (data.errorMessage) throw new Error(data.errorMessage);
+    return data;
+  }
+  public async getComingSoonMovies() {
+    const { data } = await this.API.get<IResponseComingSoonMovies>(this.endpoints.comingSoon);
+    if (data.errorMessage) throw new Error(data.errorMessage);
     return data;
   }
 }
